78c9149c2250e035b67a325ca52cb617f62624da
Nathan Silverman <nzsilverman@gmail.com>
2024-06-23 14:17:55 -0700

    Setup git preferences via a script
    It's annoying to have to set up the same aliases/ preferences I like on
    each machine. It isn't easy to track the ~/.gitconfig file because it
    contains different email addresses on different machines.

---
+++ .setup_git_preferences.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -ex
+
+# This script sets up common git preferences that I like
+# The global ~/.gitconfig can't easily be tracked in git
+# because different email addresses are used with git
+# on different machines
+
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+
+git config --global core.editor vim
+
+git config --global push.autoSetupRemote true
+git config --global push.default current
